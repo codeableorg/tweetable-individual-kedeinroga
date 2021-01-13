c5fb0c0784aec87c08ae4bf238a4fa03d2a4e5b2
kedeinroga <keybroga@gmail.com>
2021-01-13 17:12:13 -0500

    Generate controller for tweets and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/edit'
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'tweets/index'
+  get 'tweets/show'
+  get 'tweets/edit'
+  get 'tweets/update'
+  get 'tweets/new'
+  get 'tweets/create'
+  get 'tweets/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
