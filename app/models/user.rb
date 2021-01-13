8aa39239e4e4c379846e137c6278fc6ccfa5828c
kedeinroga <keybroga@gmail.com>
2021-01-13 16:20:44 -0500

    validate for tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_many :tweets
   validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
-  validates :username, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :username, uniqueness: true
+  # validates :name, presence: true
 end
