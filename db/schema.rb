c4ce2690025db4bb5f63da85eb49f6fac6ceb2e7
kedeinroga <keybroga@gmail.com>
2021-01-13 16:26:28 -0500

    generate migration for able starts at 0 by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_210123) do
+ActiveRecord::Schema.define(version: 2021_01_13_212224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_210123) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
-    t.integer "comments_count"
+    t.integer "comments_count", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
