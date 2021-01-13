f05d7be666a445c6b4cc5f0a666c5c96dd472da4
kedeinroga <keybroga@gmail.com>
2021-01-13 17:17:47 -0500

    rearranging the routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/edit'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/edit'
-  get 'tweets/update'
-  get 'tweets/new'
-  get 'tweets/create'
-  get 'tweets/destroy'
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: %i[new edit create destroy]
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
