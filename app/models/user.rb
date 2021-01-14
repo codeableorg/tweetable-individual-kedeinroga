0dda0a017676d246af46b6fe19b1c674d700d4ce
kedeinroga <keybroga@gmail.com>
2021-01-13 23:17:27 -0500

    more validations on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +8,6 @@ class User < ApplicationRecord
 
   has_many :tweets
   validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
-  validates :username, uniqueness: true
-  # validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
 end
