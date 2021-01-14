116a2593ce9fbd52990d8d450c19ed2b54d5a190
kedeinroga <keybroga@gmail.com>
2021-01-13 19:02:22 -0500

    method edit its funtional and the form edit and new created

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,17 +8,37 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+      redirect_to @tweet
+    else
+      render :edit
+    end
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to @tweet
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body, :comments_count, :user_id)
   end
 end
