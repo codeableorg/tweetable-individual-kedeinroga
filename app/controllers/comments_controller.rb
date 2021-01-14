67f3249ec806a0adea8bc43c0bf26018732f8662
kedeinroga <keybroga@gmail.com>
2021-01-13 19:56:33 -0500

    method edit and update is funtional for meodel comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,29 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to tweet_path @tweet
+    else
+      render :edit
+    end
   end
 
   def create
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :tweet_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tweets#index'
   resources :tweets do
-    resources :comments, only: %i[new edit create destroy]
+    resources :comments, only: %i[new edit update create destroy]
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
